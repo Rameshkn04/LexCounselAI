94e6ffe5a8ec9b6c1710083dc45179901b58b659
Ramesh KN <rameshkn04052004@gmail.com>
2026-06-08 19:16:49 +0530

    changed main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,28 +10,17 @@ from app.models.document import Document
 from app.api.auth import router as auth_router
 from app.api.document import router as document_router
 
-
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="LexCounsel AI",
     version="1.0.0"
 )
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-origins = [
-    "https://lex-counsel-ai-kecs.vercel.app",
-    "https://lex-counsel-ai-kecs-git-main-rameshkn04s-projects.vercel.app",
-    "http://localhost:5173",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
